336f13c9ffa61cf4d2030d18181c51ba8267553d
Thang30 <fsnthang30@gmail.com>
2017-03-03 10:16:49 +0200

    complete the competition

---
+++ helpers/compare_algorithms_helper.py
@@ -0,0 +1,33 @@
+
+# coding: utf-8
+
+# In[1]:
+
+from sklearn import model_selection
+import matplotlib.pyplot as plt
+
+def evaluate_models(models, X_train, Y_train):
+    results = []
+    names = []
+    scoring = 'accuracy'
+    for name, model in models:
+        # cross-validation
+        kfold = model_selection.KFold(n_splits=10, random_state=7)
+        cv_results = model_selection.cross_val_score(model, 
+                                                     X_train, Y_train, cv=kfold, 
+                                                     scoring=scoring)
+        
+        # store the results
+        results.append(cv_results)
+        names.append(name)
+        message = "%s: %f +/- %f" % (name, cv_results.mean(), cv_results.std())
+        print(message)
+    return results, names
+        
+def boxplot_algorithm_comparison(results, names):
+    fig = plt.figure()
+    fig.suptitle('Algorithm Comparison')
+    ax = fig.add_subplot(111)
+    plt.boxplot(results)
+    ax.set_xticklabels(names)
+
--- helpers/plotting_helpers.py
+++ helpers/plotting_helpers.py
@@ -3,16 +3,48 @@ import matplotlib.pyplot as plt
 import matplotlib.pylab as pylab
 import seaborn as sns
 
-def plot_correlation_map( df):
+
+def plot_correlation_map(df):
+    # Compute pairwise correlation of columns
+    # using Pearson correlation coefficient
     corr = df.corr()
-    _ , ax = plt.subplots( figsize =( 12 , 10 ) )
-    cmap = sns.diverging_palette( 220 , 10 , as_cmap = True )
-    _ = sns.heatmap(
-        corr, 
-        cmap = cmap,
-        square=True, 
-        cbar_kws={ 'shrink' : .9 }, 
-        ax=ax, 
-        annot = True, 
-        annot_kws = { 'fontsize' : 12 }
-    )
+    
+    fig , ax = plt.subplots(figsize = (12, 10))
+    
+    # Return a matplotlib colormap object
+    cmap = sns.diverging_palette(220 ,10 ,as_cmap = True )
+    
+    # Plot a heatmap
+    ax = sns.heatmap(corr, cmap = cmap, square = True, 
+                                 ax=ax, annot = True)
+    # Save the figure
+    fig.savefig('./figures/correlation_map.png', 
+                      bbox_inches='tight')
+
+def plot_categories(df, category, target):
+    # Plot a factorplot
+    ax = sns.factorplot(category, target, data = df)
+    ax.add_legend()
+    
+    # Save the figure
+    ax.savefig('./figures/%s distribution.png' %category, 
+                      bbox_inches='tight')
+   
+def plot_histogram(df, variable, target):
+    fig = plt.figure(figsize = (8, 6))
+    
+    # plot histogram segmented by output
+    for output, label, color in zip(range(2), ['Not Survived', 'Survived'], ['r', 'g']):
+        df[df[target] == output][variable].plot.hist(color = color, 
+                                                                            label = label,
+                                                                            alpha = 0.3, 
+                                                                            edgecolor = 'k')
+    
+    plt.xlabel('%s' %variable)
+    plt.title("The distribution of %s?" %variable)
+    plt.legend(loc='best')
+    
+    # save the figure
+    fig.savefig('./figures/%s distribution.png' %variable, 
+                      bbox_inches='tight')
+    
